e476a3fd58676577377ae3cd4785a5abda98395a
Sahidul01 <sanachoudhury7@gmail.com>
2020-05-31 23:25:43 +0530

    Create regularExpression.py

---
+++ regularExpression.py
@@ -0,0 +1,27 @@
+"""
+You are given a string .
+Your task is to find out whether
+
+is a valid regex or not.
+
+Input Format
+
+The first line contains integer
+, the number of test cases.
+The next lines contains the string
+
+.
+
+Constraints
+
+Output Format
+
+Print "True" or "False" for each test case without quotes.
+___CODES___
+"""
+import re
+for _ in range(int(input())):  
+    try:
+        print(bool(re.compile(input())))
+    except:
+        print('False')
